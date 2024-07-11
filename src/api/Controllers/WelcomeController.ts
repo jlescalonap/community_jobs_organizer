3d999084d66764b6d8e2540e2f5b22d5922fcc40
DESKTOP-VEG53QN\louis <lsarmiento@overcom.net>
2024-07-11 12:39:56 -0400

    rutas controladores e implementos

---
+++ src/api/Controllers/WelcomeController.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from 'express';
+import { WelcomeImplement } from '../Implements/WelcomeImplement';
+//importar conexion
+/**
+ *En el controlador se va a validar la data y los errores 
+ *
+ * @class WelcomeController
+ */
+export class WelcomeController {
+
+  public welcomeImplement = new WelcomeImplement()
+
+  /**
+   *LLAMA EL IMPLEMENTO  Welcomeimplement para usar sus metodos
+   *
+   * @param {Request} req
+   * @param {Response} res
+   * @memberof WelcomeController
+   */
+  async great(req: Request, res: Response) {
+
+    try {
+
+      let body = req.body;
+      const conn = 'Esto seria la conexión db';
+      let data = this.welcomeImplement.greet(conn, 'Hola bienvenido a mi Back')
+
+      //enviamos respuesta
+      res.send(data);
+      
+    } catch (error) {
+      //controlamos errores
+      res.send(error);
+    }
+    
+  }
+}
